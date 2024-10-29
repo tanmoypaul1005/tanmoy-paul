56712fe73bd95f153c9c10a14a194909a17db29a
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-10-29 21:36:43 +0600

    refactor: Update duration in experiences.js to reflect current employment status

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -2,7 +2,7 @@ const experiences = [
   {
     designation: "Software Engineer",
     company: "Qzency Ltd",
-    duration: "September 2024 - july",
+    duration: "September 2024 - PRESENT",
     location: "Pune, India",
     description:
       "As a Full Stack Developer, I have a proven track record of success, having developed more than 5 web applications that seamlessly integrate backend APIs using modern tech stacks and adhering to the highest standards of clean code practices. My commitment to efficiency is reflected in my work, where I have streamlined workflows across various projects, eliminating redundant data requirements and significantly improving overall efficiency. One of my standout achievements includes achieving an impressive 80% faster website loading time. I accomplished this by optimizing images, minimizing main-thread work, and implementing robust SEO strategies to enhance user experience and website performance. In addition to my development skills, I've also demonstrated my expertise in both backend and frontend development by resolving over 100 bugs. My thorough and comprehensive code reviews showcase my dedication to delivering high-quality solutions that not only meet but exceed expectations. As a full-stack developer, I am passionate about driving innovation and excellence in every project I undertake.",
